2246bf12720b5ff0398ed02835d7b8f744747728
S. M. Abdul Aziz <abdulaziz.aziz99@gmail.com>
2026-07-26 01:00:30 +0600

    Update footer to display current year dynamically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-
 export default function Footer() {
   return (
+    const currentYear = new Date().getFullYear();
     <footer>
       <div className="container">
         <div className="st-copyright-wrap text-center">
-          <div className="st-copyright-text">© 2025. Created by Farhana. All right reserved.</div>
+          <div className="st-copyright-text">copyright &copy; {currentYear}. Created by Farhana. All right reserved.</div>
         </div>
       </div>
     </footer>
